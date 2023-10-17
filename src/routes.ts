d9cfa0f8373bc0ceb90f34334aea4ba98f34b545
Tornike Machitidze <Tornike_Machitidze@epam.com>
2023-10-18 03:44:16 +0400

    add configuration of auth middleware and add verifyToken middleware for every /api routes, /api*

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,19 @@
 import { Express } from 'express';
 import userRouter from './controller/user.controller';
+import { ICurrentUser } from './middleware/auth.middleware';
+import { verifyToken } from './middleware/auth.middleware';
+declare global {
+  namespace Express {
+    interface Request {
+      user: ICurrentUser;
+    }
+  }
+}
 
 const setupRoutes = (app: Express) => {
-  app.use('/api/user', userRouter);
+  app.use('/user', userRouter);
+
+  app.use('/api', verifyToken);
 };
 
 export default setupRoutes;
